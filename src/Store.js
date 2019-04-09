484a4ca6bbbd72931f55fd122f668f65c9fbda0d
Richard Davis <rdavis.bacs@gmail.com>
2019-04-09 10:00:47 -0400

    store favorites in localStorage

--- src/Store.js
+++ src/Store.js
@@ -5,25 +5,38 @@ import React, { createContext, useReducer } from 'react';
 // Created via the React.createContext() function
 const Store = createContext();
 
+// get/set favorites in local browser storage
+const getFavorites = favorites => {
+  const result = localStorage.getItem('favorites');
+  if (!result) return [];
+  return JSON.parse(result);
+};
+
+const setFavorites = favorites => {
+  localStorage.setItem('favorites', JSON.stringify(favorites));
+};
+
 const initialState = {
   episodes: [],
-  favorites: []
+  favorites: getFavorites()
 };
 
 // reducer function
 // Input: state, action
-// Output: updated state object
+// Output: updated state object, updated localStorage
 const reducer = (state, action) => {
+  let favorites;
   switch (action.type) {
     case 'ADD_FAVORITE':
-      return { ...state, favorites: [...state.favorites, action.payload] };
+      favorites = [...state.favorites, action.payload];
+      setFavorites(favorites);
+      return { ...state, favorites };
     case 'FETCH_DATA':
       return { ...state, episodes: action.payload };
     case 'REMOVE_FAVORITE':
-      return {
-        ...state,
-        favorites: state.favorites.filter(item => item.id !== action.payload.id)
-      };
+      favorites = state.favorites.filter(id => id !== action.payload);
+      setFavorites(favorites);
+      return { ...state, favorites };
     default:
       return state;
   }
