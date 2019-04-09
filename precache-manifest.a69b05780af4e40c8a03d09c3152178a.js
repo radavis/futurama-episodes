1b323c94da545f0d37a8549c553317b739eb5ed3
Richard Davis <rdavis.bacs@gmail.com>
2019-04-09 11:51:42 -0400

    Updates

---
+++ precache-manifest.a69b05780af4e40c8a03d09c3152178a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ae7215888a68194161",
+    "url": "/futurama-episodes/static/js/runtime~main.df0802ef.js"
+  },
+  {
+    "revision": "bc6d182420c41aa38b18",
+    "url": "/futurama-episodes/static/js/main.288d6895.chunk.js"
+  },
+  {
+    "revision": "26bfcfec899b05b6aab2",
+    "url": "/futurama-episodes/static/js/2.59cb6a05.chunk.js"
+  },
+  {
+    "revision": "bc6d182420c41aa38b18",
+    "url": "/futurama-episodes/static/css/main.f333b5b2.chunk.css"
+  },
+  {
+    "revision": "3b74a086120dc063b94aa4222f116d51",
+    "url": "/futurama-episodes/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/futurama-episodes/precache-manifest.a69b05780af4e40c8a03d09c3152178a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/futurama-episodes/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
